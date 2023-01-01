d5e5031e17550c8e89b657a4af9172ff4adc8db8
mostafa-sadeghi <msai.sadeghi@gmail.com>
2023-01-01 19:37:37 +0330

    11dey

--- calc.py
+++ calc.py
@@ -1,6 +1,32 @@
 import tkinter as tk
 
-from tkinter import ttk,END
+from tkinter import ttk, END
+
+
+def button_equal():
+    second_number = e.get()
+    e.delete(0, END)
+    if math == "+":
+        e.insert(0, f_num + float(second_number))
+    elif math == "-":
+        e.insert(0, f_num - float(second_number))
+    elif math == "*":
+        e.insert(0, f_num * float(second_number))
+    elif math == "/":
+        e.insert(0, f_num / float(second_number))
+
+
+def button_add():
+    first_number = e.get()
+    global f_num
+    global math
+    math = "+"
+    f_num = float(first_number)
+    e.delete(0, END)
+
+
+def button_clear():
+    e.delete(0, END)
 
 
 def button_click(number):
@@ -9,31 +35,74 @@ def button_click(number):
     e.insert(0, current + str(number))
 
 
+def button_subtract():
+    first_number = e.get()
+    global f_num
+    global math
+    math = "-"
+    f_num = float(first_number)
+    e.delete(0, END)
+
+
+def button_multiply():
+    first_number = e.get()
+    global f_num
+    global math
+    math = "*"
+    f_num = float(first_number)
+    e.delete(0, END)
+
+
+def button_division():
+    first_number = e.get()
+    global f_num
+    global math
+    math = "/"
+    f_num = float(first_number)
+    e.delete(0, END)
+
+
 root = tk.Tk()
+root.columnconfigure(0, weight=1)
+style = ttk.Style(root)
+style.theme_use('clam')
+style.configure("TButton", bordercolor="silver", width=2,
+                borderwidth=2, relief="solid", font=15, background="lightblue", padding=(1, 1))
+style.configure("TEntry", bordercolor="black", padding=(25, 5), fieldbackground="lightblue",
+                borderwidth=5, relief="solid", font=15)
+style.configure("TFrame", bordercolor="green", relief="solid")
+print(style.layout("TFrame"))
+print(style.element_options("Frame.border"))
+# print(style.lookup("TButton", "font"))
+
 root.title('calculator')
-e = tk.Entry(root, width=35, borderwidth=5)
+
+e = ttk.Entry(root, width=10)
 e.grid(row=0, column=0, columnspan=3, padx=10, pady=10)
+
+frame = ttk.Frame(root, padding=(25, 5))
+frame.grid(row=1, column=0)
 # add buttons
-button_1 = tk.Button(root, text="1", padx=40, pady=20,
-                     command=lambda: button_click(1))
-button_2 = tk.Button(root, text="2", padx=40, pady=20,
-                     command=lambda: button_click(2))
-button_3 = tk.Button(root, text="3", padx=40, pady=20,
-                     command=lambda: button_click(3))
-button_4 = tk.Button(root, text="4", padx=40, pady=20,
-                     command=lambda: button_click(4))
-button_5 = tk.Button(root, text="5", padx=40, pady=20,
-                     command=lambda: button_click(5))
-button_6 = tk.Button(root, text="6", padx=40, pady=20,
-                     command=lambda: button_click(6))
-button_7 = tk.Button(root, text="7", padx=40, pady=20,
-                     command=lambda: button_click(7))
-button_8 = tk.Button(root, text="8", padx=40, pady=20,
-                     command=lambda: button_click(8))
-button_9 = tk.Button(root, text="9", padx=40, pady=20,
-                     command=lambda: button_click(9))
-button_0 = tk.Button(root, text="0", padx=40, pady=20,
-                     command=lambda: button_click(0))
+button_1 = ttk.Button(frame, text="1",
+                      command=lambda: button_click(1))
+button_2 = ttk.Button(frame, text="2",
+                      command=lambda: button_click(2))
+button_3 = ttk.Button(frame, text="3",
+                      command=lambda: button_click(3))
+button_4 = ttk.Button(frame, text="4",
+                      command=lambda: button_click(4))
+button_5 = ttk.Button(frame, text="5",
+                      command=lambda: button_click(5))
+button_6 = ttk.Button(frame, text="6",
+                      command=lambda: button_click(6))
+button_7 = ttk.Button(frame, text="7",
+                      command=lambda: button_click(7))
+button_8 = ttk.Button(frame, text="8",
+                      command=lambda: button_click(8))
+button_9 = ttk.Button(frame, text="9",
+                      command=lambda: button_click(9))
+button_0 = ttk.Button(frame, text="0",
+                      command=lambda: button_click(0))
 button_7.grid(row=1, column=0)
 button_8.grid(row=1, column=1)
 button_9.grid(row=1, column=2)
@@ -46,14 +115,22 @@ button_3.grid(row=3, column=2)
 button_0.grid(row=4, column=0)
 
 
-button_add = tk.Button(root, text="+", padx=39, pady=20, command=button_click)
-button_equal = tk.Button(root, text="=", padx=91,
-                         pady=20, command=button_click)
-button_clear = tk.Button(root, text="clear", padx=79,
-                         pady=20, command=button_click)
+button_add = ttk.Button(frame, text="+", command=button_add)
+button_equal = ttk.Button(frame, text="=", command=button_equal)
+button_clear = ttk.Button(frame, text="clear", command=button_clear)
 
-button_clear.grid(row=4, column=1, columnspan=2)
+button_clear.grid(row=4, column=1, columnspan=2, sticky="ew")
 button_add.grid(row=5, column=0)
-button_equal.grid(row=5, column=1, columnspan=2)
+button_equal.grid(row=5, column=1, columnspan=2, sticky="ew")
+
+button_sub = ttk.Button(frame, text="-", command=button_subtract)
+button_mul = ttk.Button(frame, text="×", command=button_multiply)
+button_div = ttk.Button(frame, text="÷", command=button_division)
+
+button_sub.grid(row=6, column=0)
+button_mul.grid(row=6, column=1)
+button_div.grid(row=6, column=2)
+
+
 # end button widgets
 root.mainloop()
