791ff16558810a64658b9eb1a92b8f8b4f401308
mostafa-sadeghi <msai.sadeghi@gmail.com>
2023-01-08 17:23:10 +0330

    18deyf

---
+++ tkinter_projects/new_calculator.py
@@ -0,0 +1,16 @@
+# about eval function
+# x = '1+2*3/2'
+# z = eval(x)
+# print(z)
+
+
+import tkinter as tk
+from tkinter import ttk
+
+root = tk.Tk()
+root.title('Calculator')
+root.iconbitmap('./tkinter_projects/favicon.ico')
+root.geometry('320x500')
+
+
+root.mainloop()
