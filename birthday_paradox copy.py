fe2ffd3a8993a6fafb931515266a26ac20a7148f
mostafa-sadeghi <msai.sadeghi@gmail.com>
2023-03-23 20:00:18 +0430

    sadeghi

--- birthday_paradox copy.py
+++ birthday_paradox copy.py
@@ -1,10 +1,8 @@
 
 import timeit
-import random
-import datetime
-
-
-def getBirthdays(numberOfBirthdays):
+setup = '''import random
+import datetime'''
+my_code = '''def getBirthdays(numberOfBirthdays):
     birthdays = []
     for i in range(numberOfBirthdays):
         startOfYear = datetime.date(2023, 1, 1)
@@ -19,11 +17,10 @@ def getBirthdays(numberOfBirthdays):
 
     return birthdays
 
-
 def check_equality(birthdays):
     if len(birthdays) == len(set(birthdays)):
         return None
-
+    
     # for i in range(len(birthdays)):
     #     for j in range(i+1, len(birthdays)):
     #         if birthdays[i] == birthdays[j]:
@@ -33,10 +30,20 @@ def check_equality(birthdays):
         if birthdays[i] in birthdays[i+1:]:
             return birthdays[i]
 
+    
 
 print("enter how many birthday you want? ")
 #number_of_birthdays = int(input('> '))
 #all_birthdays = getBirthdays(number_of_birthdays)
 all_birthdays = getBirthdays(23)
 print("all_birthdays", all_birthdays)
-print(check_equality(all_birthdays))
+print([check_equality(all_birthdays)])
+'''
+
+print(timeit.timeit(stmt=my_code, setup=setup, number=10000))
+
+
+# numbers = [1,2,3,4,5,6]
+
+# if 1 in numbers:
+#     print('exists')
--- comare_list_items.py
+++ comare_list_items.py
@@ -1,9 +1,36 @@
-list1 = [1, 2, 3, 4, 5, 6, 7, 5, 5, 5]
+import timeit
 
-for i in range(len(list1)):
-    print("i = ", i)
-    for j in range(i+1, len(list1)):
-        print("j = ", j)
+
+# print(list1)
+
+code1 = '''list1 = [1, 2, 3, 4, 5, 6, 7, 8, 9]*100000
+def is_unique1(my_list):
+    for i in range(len(my_list)):
+        for j in range(i+1, len(my_list)):
+            if my_list[i] == my_list[j]:
+                return 'YES'
+    return 'NO'
+
+
+is_unique1(list1)
+'''
+
+
+code2 = '''list1 = [1, 2, 3, 4, 5, 6, 7, 8, 9]*100000
+def is_unique2(my_list):
+    for i in range(len(my_list)):
+
+        if my_list[i] in my_list[i+1:]:
+            return 'YES'
+    return 'NO'
+
+
+is_unique2(list1)
+'''
+
+print('code 1', timeit.timeit(stmt=code1, number=1000))
+print('code 2', timeit.timeit(stmt=code2, number=1000))
+
 
 # با استفاده از کدهای بالا تابعی بنویس که
 # یک لیست را به عنوان ورودی دریافت نماید و
