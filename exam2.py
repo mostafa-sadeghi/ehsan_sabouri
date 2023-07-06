03abdedba0d2368b15c5a3f912ff5a1052371581
msa <msai.sadehi@gmail.com>
2023-07-06 19:59:05 +0330

    sadehgi

---
+++ exam2.py
@@ -0,0 +1,60 @@
+# x = [1, 2, 3, 4, 5, 9]
+# print(f"first item : {x[0]}")
+# print(f"first item : {x[-1]}")
+# print(f"first item : {x[5]}")
+# print(f"first item : {x[len(x) - 1]}")
+# print(f"first slice : {x[:3]}")
+# print(f"second slice : {x[3:]}")
+
+# print(f"sum of all numbers: {sum(x)}")
+
+# total = 0
+# for number in x:
+#     total += number
+# print(f"sum of all numbers: {total}")
+
+# total = 0
+# for i in range(len(x)):
+#     total += x[i]
+# print(f"sum of all numbers: {total}")
+
+# total = 0
+# for item in enumerate(x):
+#     total += item[1]
+
+# print(f"sum of all numbers: {total}")
+
+# i = 0
+# total = 0
+# while i < len(x):
+#     total += x[i]
+#     i += 1
+
+# print(f"sum of all numbers: {total}")
+
+
+x = [1, 2, 3, 4, 5, 9]
+
+
+# number = int(input("enter a number: "))
+# exists_or_not = False
+# for n in x:
+#     if n == number:
+#         x.remove(number)
+#         exists_or_not = True
+# if not exists_or_not:
+#     print(f"{number} does not exists.")
+
+
+# print(f"x = {x}")
+
+my_numbers = []
+total = 0
+for number in range(100, 251):
+    if number % 3 == 0:
+        my_numbers.append(number)
+        if number % 2 != 0:
+            total += number
+
+print(my_numbers)
+print(total)
